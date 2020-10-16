f5125073d653e1d7cee0cad7572ad7011283cce6
Mustafa <mustafaanvarov@mail.ru>
2020-10-16 22:54:54 +0500

    2_23 Основы ООП

--- script.js
+++ script.js
@@ -1,95 +1,31 @@
 'use strict';
 
-// let a = 5,
-//     b = a;
-    
-// b = b + 5;
+let str = 'some';
+let strObj = new String(str);
 
-// console.log(b);
-// console.log(a);
+console.log(typeof(str));
+console.log(typeof(strObj));
 
-// const obj = {
-//     a: 5,
-//     b: 1
+console.dir([1, 2, 3]);
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function () {
+        console.log('Hello');
+    }
+};
+
+const john = Object.create(soldier);
+
+// const john = {
+//     health: 100
 // };
 
-// const copy = obj; //Ссылку
+// john.__proto__ = soldier;
 
-// copy.a = 10;
+// Object.setPrototypeOf(john, soldier);
 
-// console.log(copy);
-// console.log(obj);
+// // console.log(john.sayHello);
+john.sayHello();
 
-function copy(mainObj) {
-    let objCopy = {};
-    
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-    
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
-};
-
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-
-console.log(newNumbers);
-console.log(numbers);
-
-const add = {
-  d: 17,
-  e: 20  
-};
-
-const cloner = Object.assign({}, add);
-
-cloner.d = 20;
-
-// console.log(add);
-// console.log(cloner);
-
-const oldArr = ['a', 'b', 'c'];
-const newArr = oldArr.slice();
-
-newArr[1] = 'asdsdfsdf';
-console.log(newArr);
-console.log(oldArr);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejournal', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];
-      
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-const array = ['a', 'b'];
-
-const newArray = [...array];
-
-const q = {
-    one: 1,
-    two: 2
-};
-const newObj = {...q};
-// changed
