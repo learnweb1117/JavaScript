e0b725c62e66b600075ebaaa73113259f3c68ebe
Mustafa <mustafaanvarov@mail.ru>
2020-10-21 18:26:05 +0500

    3_37 ClassList и делегирование событий

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 'use strict';
 
-// touchstart
-// touchmove
-// touchend
-// touchenter
-// touchleave
-// touchcancel
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
 
-window.addEventListener('DOMContentLoaded', () => {
-   const box = document.querySelector('.box');
-   
-   box.addEventListener('touchstart', (e) => {
-       e.preventDefault();
-       
-       console.log('Start');
-       console.log(e.changedTouches);
-   });
-   
-   box.addEventListener('touchmove', (e) => {
-    e.preventDefault();
-    
-    console.log(e.targetTouches[0].pageX);
-});
-   box.addEventListener('touchend', (e) => {
-    e.preventDefault();
-    
-    console.log('End');
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(0));
+// console.log(btns[1].classList.add('red', 'nextClass'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
+
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// }
+
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')) {
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
+    btns[1].classList.toggle('red');
 });
 
+// console.log(btns[0].className);
+
+wrapper.addEventListener('click', (e) => {
+    if (e.target && e.target.matches('button.red')) {
+        console.log('Hello');
+    }
 });
 
-// touches
-// targetTouches
-// changedTouches
+// btns.forEach(btn => {
+//    btn.addEventListener('click', () => {
+//      console.log(1);  
+//    });
+// });
+
+
+    const btn = document.createElement('button');
+btn.classList.add('red');
+    wrapper.append(btn);
